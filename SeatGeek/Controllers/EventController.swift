71655d2f8c54bca4de13899f4c987aa1e6c09922
Vici Shaweddy <vshaweddy@gmail.com>
2021-02-27 21:04:25 -0500

    added search method

--- SeatGeek/Controllers/EventController.swift
+++ SeatGeek/Controllers/EventController.swift
@@ -56,4 +56,44 @@ class EventController {
             }
         }.resume()
     }
+    
+    func searchQuery(with searchTerm: String, completion: @escaping (Result<[EventRepresentation], NetworkError>) -> Void) {
+        baseURL.queryItems = [
+            URLQueryItem(name: "client_id", value: clientID),
+            URLQueryItem(name: "q", value: searchTerm)
+        ]
+        
+        guard let url = baseURL.url else { return }
+        
+        var request = URLRequest(url: url)
+        request.httpMethod = HTTPMethod.get.rawValue
+        
+        
+        URLSession.shared.dataTask(with: request) { (data, response, error) in
+            if let response = response as? HTTPURLResponse,
+               response.statusCode != 403 {
+                completion(.failure(.badAuth))
+            }
+            
+            if error != nil {
+                completion(.failure(.otherError))
+            }
+            
+            guard let data = data else {
+                completion(.failure(.badData))
+                return
+            }
+            
+            let decoder = JSONDecoder()
+            
+            do {
+                let itemSearch = try decoder.decode(EventsResponse.self, from: data)
+                completion(.success(itemSearch.events))
+            } catch {
+                print("Error decoding search results: \(error)")
+                completion(.failure(.noDecode))
+                return
+            }
+        }.resume()
+    }
 }
